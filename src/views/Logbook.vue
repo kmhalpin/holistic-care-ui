b7a7696cd021a59d9db317b48f429422d9577986
kmhalpin <kmhapinn3@gmail.com>
2021-12-08 11:24:32 +0700

    views

---
+++ src/views/Logbook.vue
@@ -0,0 +1,155 @@
+<template>
+  <layout>
+    <card title="Insert Logbook">
+      <form>
+        <Field label="Kompetensi">
+          <v-select
+            v-model="isKompetensi" :options="['Ya', 'Tidak']">
+          </v-select>
+        </Field>
+        <input type="hidden" v-model="isKompetensi" name="isKompetensi" />
+        <Field label="Logbook" v-if="isKompetensi === 'Tidak'">
+          <Input name="logbook" type="textarea" />
+        </Field>
+        <Field label="List Kompetensi" v-if="isKompetensi === 'Ya'">
+          <v-select
+            v-model="kompetensi" :options="listKompetensi">
+          </v-select>
+        </Field>
+        <input type="hidden" v-model="kompetensi" name="kompetensi" />
+        <Field label="Tipe" v-if="isKompetensi === 'Ya'">
+          <check-input
+            v-model="tipeKompetensi"
+            name="tipeKompetensi"
+            type="radio"
+            column
+            :options="{ 1: 'Observasi', 2: 'Asistensi', 3: 'Mandiri' }"
+          />
+        </Field>
+        <Field label="Keterangan" v-if="isKompetensi === 'Ya'">
+          <Input name="keterangan" type="textarea" />
+        </Field>
+        <jb-buttons type="justify-end">
+          <jb-button
+            type="submit"
+            color="bg-blue-500 text-white hover:bg-blue-600"
+            label="Submit"
+          />
+        </jb-buttons>
+      </form>
+    </card>
+    <card title="Logbook">
+      <Table :rows="logbooks">
+        <template v-slot:thead>
+          <tr>
+            <th>No</th>
+            <th>Logbook</th>
+            <th>Kompetensi</th>
+          </tr>
+        </template>
+        <template v-slot:row="rowProps">
+          <td>{{ rowProps.index + 1 }}</td>
+          <td>{{ rowProps.item.logbook }}</td>
+          <td>{{ rowProps.item.kompetensi ? 'Y' : 'N' }}</td>
+        </template>
+      </Table>
+    </card>
+  </layout>
+</template>
+
+<script>
+import vSelect from 'vue-select';
+import Card from '../components/Card.vue';
+import Table from '../components/Table.vue';
+import Field from '../components/Field.vue';
+import Input from '../components/Input.vue';
+import JbButtons from '../components/JbButtons.vue';
+import JbButton from '../components/JbButton.vue';
+import CheckInput from '../components/CheckInput.vue';
+import Layout from '../layout/Layout.vue';
+
+export default {
+  name: 'Logbook',
+  data() {
+    return {
+      isKompetensi: 'Ya',
+      listKompetensi: [
+        'Memasang Infus Pasien',
+        'Memberi Obat Pasien',
+        'Memberi Makan Pasien',
+        'Memberi Minum Pasien',
+      ],
+      kompetensi: '',
+      tipeKompetensi: '',
+      logbooks: [
+        {
+          id: 0,
+          logbook: 'Absensi',
+          kompetensi: false,
+        },
+        {
+          id: 1,
+          logbook: 'Upacara',
+          kompetensi: false,
+        },
+        {
+          id: 2,
+          logbook: 'Memasang Infus Pasien A',
+          kompetensi: true,
+        },
+        {
+          id: 3,
+          logbook: 'Memberi Obat Pasien',
+          kompetensi: true,
+        },
+        {
+          id: 11,
+          logbook: 'Memberi Obat Pasien',
+          kompetensi: true,
+        },
+        {
+          id: 22,
+          logbook: 'Memberi Obat Pasien',
+          kompetensi: true,
+        },
+        {
+          id: 23,
+          logbook: 'Memberi Obat Pasien',
+          kompetensi: true,
+        },
+        {
+          id: 13,
+          logbook: 'Memberi Obat Pasien',
+          kompetensi: true,
+        },
+        {
+          id: 17,
+          logbook: 'Memberi Obat Pasien',
+          kompetensi: true,
+        },
+        {
+          id: 2231,
+          logbook: 'Memberi Obat Pasien',
+          kompetensi: true,
+        },
+        {
+          id: 112,
+          logbook: 'Memberi Obat Pasien',
+          kompetensi: true,
+        },
+      ],
+    };
+  },
+  components: {
+    Layout,
+    Card,
+    Table,
+    Field,
+    Input,
+    vSelect,
+    CheckInput,
+    JbButtons,
+    JbButton,
+  },
+};
+</script>
