d2fa39924e879d8d6f7722ecb379a8b8ea6d904d
kmhalpin <kmhapinn3@gmail.com>
2022-01-14 21:37:59 +0700

    use intl

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,5 @@
-export const getDateString = (dateISO) => {
-  const date = new Date(dateISO);
-  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agu', 'Sep', 'Okt', 'Nov', 'Des'];
-  return `${date.getDate() - 1} ${months[date.getMonth()] || 'Not Found'} ${date.getFullYear()}`;
-};
+const dateFormat = new Intl.DateTimeFormat('id', { dateStyle: 'medium', timeZone: 'GMT' });
+
+export const getDateString = (dateISO) => dateFormat.format(Date.parse(dateISO));
 
 export default { getDateString };
